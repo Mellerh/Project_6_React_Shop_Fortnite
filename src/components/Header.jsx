371336a0bc48efef11450b337b2c557fd48fec86
Mellerh <103604450+Mellerh@users.noreply.github.com>
2023-07-12 10:40:46 +0300

    new titile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <nav className="pink darken-2">
             <div className="nav-wrapper">
                 <span className="logoName">
-                    <a href="" Name="brand-logo">React Fortnite shop</a>
+                    <a href="" Name="brand-logo">React Fortnite shop context+reducer</a>
                 </span>
                 
             </div>
